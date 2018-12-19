317b3542d0ed9a9db3efff14dc5554072b876e6c
y4shiro <yamashiro_naoki@ca-adv.co.jp>
2018-12-19 16:41:01 +0900

    7.3.4 演習4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,5 +12,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
+    assert_select "form[action=?]", signup_path
   end
 end
